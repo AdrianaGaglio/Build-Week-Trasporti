b1ccaccc62d86e42752acad41ffbfa7340c06c30
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-12-09 15:23:43 +0100

    updated mainInsert

--- src/main/java/insert_data/MainInsertBiglietto.java
+++ src/main/java/insert_data/MainInsertBiglietto.java
@@ -1,5 +1,6 @@
 package insert_data;
 
+import com.github.javafaker.Faker;
 import epicode.it.dao.AbbonamentoDAO;
 import epicode.it.dao.GiornalieroDAO;
 import epicode.it.entities.biglietto.Abbonamento;
@@ -11,20 +12,22 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 import java.time.LocalDate;
+import java.util.Locale;
 
 public class MainInsertBiglietto {
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("trasporti");
+        Faker faker = new Faker(new Locale("it"));
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("unit-jpa");
         EntityManager em = emf.createEntityManager();
 
         // PRENDO I METODI DALLE DAO
-        GiornalieroDAO giornalieroDAO = new GiornalieroDAO();
-        AbbonamentoDAO abbonamentoDAO = new AbbonamentoDAO();
+        GiornalieroDAO giornalieroDAO = new GiornalieroDAO(em);
+        AbbonamentoDAO abbonamentoDAO = new AbbonamentoDAO(em);
 
         try {
             // CREO UN NUOVO BIGLIETTO GIORNALIERO
             Giornaliero giornaliero = new Giornaliero();
-            giornaliero.setScadenza();
+            giornaliero.setScadenza(LocalDate.now());
             giornaliero.setDa_attivare(true);
 
             // CREO UN NUOVO ABBONAMENTO
