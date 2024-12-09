c8f8a0d561251d3c9b6c8b7088f6b694fb743e26
Giacomo Rossettini <rossegiac@gmail.com>
2024-12-09 12:52:29 +0100

    classi varie

--- src/main/java/epicode/it/entities/biglietto/Abbonamento.java
+++ src/main/java/epicode/it/entities/biglietto/Abbonamento.java
@@ -1,4 +1,18 @@
 package epicode.it.entities.biglietto;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity
 public class Abbonamento extends Biglietto{
+    private Periodicy periodicy;
+    private boolean attivo;
+    // private List<Tratta> tratte;
+    private String tariffa;
+
+    // @ManyToOne
+    // Tessera tessera
+    // Tratta tratta
 }
--- src/main/java/epicode/it/entities/biglietto/Giornaliero.java
+++ src/main/java/epicode/it/entities/biglietto/Giornaliero.java
@@ -1,4 +1,12 @@
 package epicode.it.entities.biglietto;
 
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+@Entity
 public class Giornaliero extends Biglietto{
+    private boolean da_attivare;
+    // Mezzo mezzo;
+    // Tratta tratta;
 }
