ef1f7aaec3207ba9153140b11affe09139d8ff16
erikagarofalo <erika.garofalo@gmail.com>
2024-12-10 14:41:22 +0100

    implementato metodo tratta e gestore percorrenze

--- src/main/java/epicode/it/entities/tratta/Tratta.java
+++ src/main/java/epicode/it/entities/tratta/Tratta.java
@@ -5,8 +5,9 @@ import epicode.it.entities.percorrenza.Percorrenza;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.sql.Time;
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -17,13 +18,21 @@ public class Tratta {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-
     private String partenza;
-
     private String capolinea;
-
     private LocalTime durata;
 
+    @OneToMany(mappedBy = "tratta")
+    private List<Percorrenza> percorrenze = new ArrayList<>();
 
+    public Percorrenza percorriTratta(List<Mezzo> mezzi, LocalDate data) {
+        Percorrenza nuovaPercorrenza = new Percorrenza();
+        nuovaPercorrenza.setTratta(this);
+        nuovaPercorrenza.setMezzi(mezzi);
+        nuovaPercorrenza.setData(data);
+        nuovaPercorrenza.setDurata_effettiva(this.durata);
 
+        this.percorrenze.add(nuovaPercorrenza);
+        return nuovaPercorrenza;
+    }
 }
