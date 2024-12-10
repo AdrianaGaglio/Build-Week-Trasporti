b8e520a1d5be22e559d9ea70bd7a39a139cdb13b
Adriana Gaglio <adriana.gaglio@gmail.com>
2024-12-10 09:41:26 +0100

    create maintest with test cases

---
+++ src/main/java/insert_data/MainTest.java
@@ -0,0 +1,34 @@
+package insert_data;
+
+import com.github.javafaker.Faker;
+import epicode.it.dao.RivAutomaticoDAO;
+import epicode.it.dao.RivenditoreDAO;
+import epicode.it.dao.utente.UtenteDAO;
+import epicode.it.entities.rivenditore.RivAutomatico;
+import epicode.it.entities.rivenditore.RivFisico;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.sql.Time;
+import java.util.Locale;
+
+public class MainTest {
+    public static void main(String[] args) {
+        Faker faker = new Faker(new Locale("it"));
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("unit-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        // 1. creare N rivenditori
+
+        // 2. creare N utenti
+
+        // 3. creare alcune tessere da associare agli utenti
+
+        // 4. emettere alcuni biglietti per diversi rivenditori
+
+        // 5. emettere alcuni abbonamenti per diversi rivenditori associati a delle tessere
+
+
+    }
+}
